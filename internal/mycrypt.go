22751a657c2d29f036caaedd43c15e858fcd8405
netzen86 <netzen0su@gmail.com>
2024-12-08 19:05:44 +0500

    complete crypt and decrypt

--- internal/mycrypt.go
+++ internal/mycrypt.go
@@ -7,84 +7,28 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
-	"log"
 	"os"
 )
-
-type Consumer struct {
-	file *os.File
-	// добавляем Reader в Consumer
-	// reader  *bufio.Reader
-	Scanner *bufio.Scanner
-}
-
-func NewConsumer(filename string) (*Consumer, error) {
-	file, err := os.OpenFile(filename, os.O_RDONLY, 0666)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Consumer{
-		file: file,
-		// создаём новый Reader
-		Scanner: bufio.NewScanner(file),
-	}, nil
-}
-
-func (c *Consumer) ReadPrivKey() (*rsa.PrivateKey, error) {
-	var key *rsa.PrivateKey
-	var err error
-	date := make([]byte, 0, 2048)
-	scanner := c.Scanner
-	for scanner.Scan() {
-		date = append(date, scanner.Bytes()...)
-	}
-	log.Println(scanner.Bytes())
-	block, _ := pem.Decode(date)
-
-	if block == nil || block.Type != "PUBLIC KEY" {
-		log.Fatal("failed to decode PEM block containing public key")
-	}
-
-	key, err = x509.ParsePKCS1PrivateKey(block.Bytes)
-	if err != nil {
-		return nil, fmt.Errorf("err when parse priv key %w", err)
-	}
-	return key, nil
-}
-
-func (c *Consumer) ReadPubKey() (*rsa.PublicKey, error) {
-	var pub any
-	var err error
-	scanner := c.Scanner
-	for scanner.Scan() {
-		// преобразуем данные из JSON-представления в структуру
-		block, _ := pem.Decode([]byte(scanner.Bytes()))
-		pub, err = x509.ParsePKIXPublicKey(block.Bytes)
-		if err != nil {
-			return nil, fmt.Errorf("err when parse pub key %w", err)
-		}
-	}
-
-	switch pub := pub.(type) {
-	case *rsa.PublicKey:
-		return pub, nil
-	default:
-		return nil, fmt.Errorf("only rsa pubkey allowed")
-	}
-}
 
 func ReadPrivedKey(filename string) (*rsa.PrivateKey, error) {
 	var key *rsa.PrivateKey
 
-	data, err := os.ReadFile(filename)
+	KeyFile, err := os.Open(filename)
 	if err != nil {
 		return nil, fmt.Errorf("error when read priv key file %w", err)
 	}
-	log.Println(string(data))
-	block, _ := pem.Decode(data)
+	defer KeyFile.Close()
 
-	if block == nil || block.Type != "PUBLIC KEY" {
+	pemfileinfo, _ := KeyFile.Stat()
+	pembytes := make([]byte, pemfileinfo.Size())
+	buffer := bufio.NewReader(KeyFile)
+	_, err = buffer.Read(pembytes)
+	if err != nil {
+		return nil, fmt.Errorf("error when read priv key file %w", err)
+	}
+	block, _ := pem.Decode(pembytes)
+
+	if block == nil {
 		return nil, fmt.Errorf("failed to decode PEM block containing public key")
 	}
 
@@ -95,13 +39,77 @@ func ReadPrivedKey(filename string) (*rsa.PrivateKey, error) {
 	return key, nil
 }
 
-func GenerateKeys(filename string) error {
+func ReadPublicKey(filename string) (*rsa.PublicKey, error) {
+	var key *rsa.PublicKey
 
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	KeyFile, err := os.Open(filename)
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("error when read priv key file %w", err)
+	}
+	defer KeyFile.Close()
+
+	pemfileinfo, _ := KeyFile.Stat()
+	pembytes := make([]byte, pemfileinfo.Size())
+	buffer := bufio.NewReader(KeyFile)
+	_, err = buffer.Read(pembytes)
+	if err != nil {
+		return nil, fmt.Errorf("error when read priv key file %w", err)
+	}
+	block, _ := pem.Decode(pembytes)
+
+	if block == nil {
+		return nil, fmt.Errorf("failed to decode PEM block containing public key")
 	}
 
-	os.WriteFile("privKey", x509.MarshalPKCS1PrivateKey(privateKey), 0666)
+	key, err = x509.ParsePKCS1PublicKey(block.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("err when parse public key %w", err)
+	}
+	return key, nil
+}
+
+func GenerateKeys() error {
+	var err error
+	var pemPrivFile, pemPubFile *os.File
+	var privateKey *rsa.PrivateKey
+	var publicKey *rsa.PublicKey
+
+	pemPrivFile, err = os.Create("private_key.pem")
+	if err != nil {
+		return fmt.Errorf("error when create private pem file %w", err)
+	}
+	defer pemPrivFile.Close()
+
+	pemPubFile, err = os.Create("public_key.pem")
+	if err != nil {
+		return fmt.Errorf("error when create public pem file %w", err)
+	}
+	defer pemPubFile.Close()
+
+	privateKey, err = rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return fmt.Errorf("error when generate priv key %w", err)
+	}
+
+	publicKey = &privateKey.PublicKey
+
+	blockPriv := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	}
+
+	blockPub := &pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: x509.MarshalPKCS1PublicKey(publicKey),
+	}
+
+	if err := pem.Encode(pemPrivFile, blockPriv); err != nil {
+		return fmt.Errorf("error when write pem format priv key to file %w", err)
+	}
+
+	if err := pem.Encode(pemPubFile, blockPub); err != nil {
+		return fmt.Errorf("error when write pem format pib key to file %w", err)
+	}
+
 	return nil
 }
